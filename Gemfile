462f66e6b8816a61c66e4345f7c936183f6be7ba
Diana Liu <dianaliu@users.noreply.github.com>
2015-04-27 20:14:42 -0400

    /shrug

--- Gemfile
+++ Gemfile
@@ -1,2 +1,9 @@
 source 'https://rubygems.org'
 gem 'sinatra'
+gem 'pg'
+gem 'activerecord'
+gem 'sinatra-activerecord'
+
+group :development do
+  gem 'tux'
+end
--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require 'sinatra/active_record'
+require ./environments
 
 get '/' do 
   "Hello World~"
+end
+
+post '/status' do
+end
+
+class Room < ActiveRecord::Base
 end
